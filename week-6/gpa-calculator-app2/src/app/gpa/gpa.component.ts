12cf75ddaa3c8b87b196eb984e087686c18c28e8
Larry Ohaka <larryohaka@ymail.com>
2021-07-03 14:20:07 -0400

    new

---
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -0,0 +1,24 @@
+/**
+ * * Title: gpa.component.ts
+ * Author: Larry Ohaka
+ * Date: 6/25/21
+ * Description: Navigation and Layout
+ */
+
+ import { Component, OnInit, Input } from '@angular/core';
+
+ @Component({
+   selector: 'app-gpa',
+   templateUrl: './gpa.component.html',
+   styleUrls: ['./gpa.component.css']
+ })
+ export class GpaComponent implements OnInit {
+
+   @Input() gpaTotal: number;
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+ }
